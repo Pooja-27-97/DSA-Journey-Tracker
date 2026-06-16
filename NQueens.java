d8cf25a114f27a2581ed3017fa06f8f74a8423d3
Pooja-27 <poojarm2797@gmail.com>
2026-06-16 20:32:18 +0530

    N Queens problem

---
+++ NQueens.java
@@ -0,0 +1,94 @@
+public class NQueens {
+    public static boolean isSafe(char board[][], int row, int col) {
+        //vertical up
+        for(int i=row-1; i >= 0 ; i--) {
+            if(board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        //left diagonal
+        for(int i=row-1, j=col-1; i >= 0 &&j >= 0; i--, j--) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        //right diagonal
+        for(int i=row-1, j=col+1; i >= 0 &&j < board.length; i--, j++) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void nQueens(char board[][], int row) {
+        //base
+        if(row == board.length) {
+            printBoard(board);
+            // count++;
+            return;
+        }
+        //column loop
+        for(int j=0; j <board.length; j++) {
+            if(isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row+1);
+                board[row][j] = 'X';
+            }
+        }
+    }
+
+    // public static boolean nQueens(char board[][], int row) {
+    //     //base
+    //     if(row == board.length) {
+    //         // printBoard(board);
+    //         count++;
+    //         return true;
+    //     }
+    //     //column loop
+    //     for(int j=0; j <board.length; j++) {
+    //         if(isSafe(board, row, j)) {
+    //             board[row][j] = 'Q';
+    //             if(nQueens(board, row+1)) {
+    //                 return true;
+    //             }
+    //             board[row][j] = 'X';
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("-------CHESS BOARD--------");
+        for(int i=0; i < board.length; i++) {
+            for(int j=0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // static int count = 0;
+
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+
+        for(int i=0; i <n; i++) {
+            for(int j=0; j < n ; j++) {
+                board[i][j] = 'X';
+            }
+        }
+
+        nQueens(board, 0);
+        // if(nQueens(board, 0)) {
+        //     System.out.println("Soluntion possible");
+        //     printBoard(board);
+        // } else {
+        //     System.out.println("Solution does not exist");
+        // }
+        // System.out.println("Total ways: " + count);
+    }
+}
