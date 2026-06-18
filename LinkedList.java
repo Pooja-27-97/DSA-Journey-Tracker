3524f3de260459bc739af4801a238985acf8dc56
Pooja-27 <poojarm2797@gmail.com>
2026-06-19 02:40:44 +0530

    Detecting and removing cycles or loop in LL

--- LinkedList.java
+++ LinkedList.java
@@ -204,6 +204,18 @@ public class LinkedList {
         return true;
     }
 
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;// cycle exists
+        }
+        return false; // cycle doesn't exist
+    }
+
     public void print() {
         // if(head == null) {
         //     System.out.println("Empty");
@@ -216,17 +228,54 @@ public class LinkedList {
         }
         System.out.println("null");
     }
+
+    public static void removeCycle() {
+        //detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)  {
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle == false) return;
+
+        //find meeting point
+        slow = head;
+        Node prev = null;
+        while(slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        //remove cycle -> last.next = null
+        prev.next = null;
+    }
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+        System.out.println(isCycle());
+        removeCycle();
+        System.out.println(isCycle());
+        // LinkedList ll = new LinkedList();
         // ll.head = new Node(1);
         // ll.head.next = new Node(2);
 
-        ll.addFirst(3);
-        ll.addFirst(2);
-        ll.addLast(4);
-        ll.addLast(5);
-        ll.print();
-        System.out.println(ll.checkPalindrome());
+        // ll.addFirst(3);
+        // ll.addFirst(2);
+        // ll.addLast(4);
+        // ll.addLast(5);
+        // ll.print();
+        // System.out.println(ll.checkPalindrome());
         // ll.deleteNthfromEnd(2);
         // ll.print();
         // ll.reverse();
