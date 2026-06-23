83afcaf81018e6237314c921b094e814b9367748
Pooja-27 <poojarm2797@gmail.com>
2026-06-24 03:03:01 +0530

    Binary trees

--- Chocola.java
+++ Chocola.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Chocola {
     public static void main(String[] args) {
-        int n = 4; int m = 6;
+        // int n = 4; int m = 6;
         Integer costVer[] = {2, 1, 3, 1, 4};
         Integer costHor[] = {4, 1, 2};
 
