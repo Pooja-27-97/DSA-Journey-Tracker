412b18cc392f16042961d7fee8e21bd7c34ffd16
Pooja-27 <poojarm2797@gmail.com>
2026-06-25 02:56:33 +0530

    Kth Ancestor of node

--- BinaryTrees.java
+++ BinaryTrees.java
@@ -264,6 +264,20 @@ public class BinaryTrees {
 
         return dist1+dist2;
     }
+
+    public static int kAncestor(Node root, int n, int k) {
+        if(root == null) return -1;
+        if(root.data == n) return 0;
+
+        int leftDist = kAncestor(root.left, n, k);
+        int rightDist = kAncestor(root.right, n, k);
+
+        if(leftDist == -1 && rightDist == -1) return -1;
+
+        int max = Math.max(leftDist, rightDist);
+        if(max+1 == k) System.out.println(root.data);
+        return max+1;
+    }
     public static void main(String[] args) {
         // int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         // BinaryTree tree = new BinaryTree();
@@ -290,8 +304,12 @@ public class BinaryTrees {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        int n1=4, n2=7;
-        System.out.println(minDist(root, n1, n2));
+
+        int n =  5, k = 2;
+        kAncestor(root, n, k);
+
+        // int n1=4, n2=7;
+        // System.out.println(minDist(root, n1, n2));
         // System.out.println(lca(root, n1, n2).data);
         // System.out.println(lca2(root, n1, n2).data);
 
