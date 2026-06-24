c2eb51da49197a7a6dcbaf05ff312780036bad78
Pooja-27 <poojarm2797@gmail.com>
2026-06-25 02:43:58 +0530

    Minimum distance between nodes

--- BinaryTrees.java
+++ BinaryTrees.java
@@ -244,6 +244,26 @@ public class BinaryTrees {
 
         return root;
     }
+
+    public static int lcaDist(Node root, int n) {
+        if(root == null) return -1;
+        if(root.data == n) return 0;
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1) return -1;
+        else if(leftDist == -1) return rightDist+1;
+        else return leftDist+1;
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1+dist2;
+    }
     public static void main(String[] args) {
         // int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         // BinaryTree tree = new BinaryTree();
@@ -270,9 +290,10 @@ public class BinaryTrees {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        int n1=6, n2=7;
+        int n1=4, n2=7;
+        System.out.println(minDist(root, n1, n2));
         // System.out.println(lca(root, n1, n2).data);
-        System.out.println(lca2(root, n1, n2).data);
+        // System.out.println(lca2(root, n1, n2).data);
 
         // int key = 2;
         // kLevel(root, 1, key);
