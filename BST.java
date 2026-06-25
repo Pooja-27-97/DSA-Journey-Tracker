0ae12d09abdd45b87b8f04a0f0c81ebeabbeae90
Pooja-27 <poojarm2797@gmail.com>
2026-06-26 01:38:36 +0530

    Balanced BST

--- BST.java
+++ BST.java
@@ -117,18 +117,84 @@ public class BST {
             preorder(root.left);
             preorder(root.right);
     }
+
+    public static Node createBST(int arr[], int st, int end) {
+        if(st > end) return null;
+        int mid = (st+end)/2;
+        Node root = new Node(arr[mid]);
+        root.left = createBST(arr, st, mid-1);
+        root.right = createBST(arr, mid+1, end);
+        return root;
+    }
+
+    public static void getInorder(Node root, ArrayList<Integer> inorder) {
+        if(root == null) return;
+
+        getInorder(root.left, inorder);
+        inorder.add(root.data);
+        getInorder(root.right, inorder);
+    }
+
+    public static Node createBST2(ArrayList<Integer> inorder, int st, int end) {
+        if(st > end) return null;
+        int mid = (st+end)/2;
+        Node root = new Node(inorder.get(mid));
+        root.left = createBST2(inorder, st, mid-1);
+        root.right = createBST2(inorder, mid+1, end);
+        return root;
+    }
+
+    public static Node balanceBST(Node root) {
+        //inorder sequence
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInorder(root, inorder);
+
+        //seq -> balanced bst
+        root = createBST2(inorder, 0, inorder.size()-1);
+        return root;
+    }
     public static void main(String[] args) {
-        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
-        Node root = null;
-        for(int i=0; i <values.length; i++) {
-            root = insert(root, values[i]);
-        }
+                //         8
+                //        / \
+                //       6   10
+                //      /     \
+                //     5       11
+                //    /         \ 
+                //   3           12
+        Node root = new Node(8);
+        root.left = new Node(6);
+        root.left.left = new Node(5);
+        root.left.left.left = new Node(3);
 
-        inorder(root);
-        System.out.println();
+        root.right = new Node(10);
+        root.right.right = new Node(11);
+        root.right.right.right = new Node(12);
 
-        root = createMirror(root);
+        root = balanceBST(root);
         preorder(root);
+
+
+        // int arr[] = {3, 5, 6, 8, 10, 11, 12};
+        //         //     8
+        //         //    / \
+        //         //   5   11
+        //         //  / \  / \
+        //         // 3   6 10 12
+        // Node root = createBST(arr, 0, arr.length-1);
+        // preorder(root);
+
+
+        // int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        // Node root = null;
+        // for(int i=0; i <values.length; i++) {
+        //     root = insert(root, values[i]);
+        // }
+
+        // inorder(root);
+        // System.out.println();
+
+        // root = createMirror(root);
+        // preorder(root);
 
         // if(isValidBST(root, null, null)) System.out.println("Valid");
         // else System.out.println("Not valid");
