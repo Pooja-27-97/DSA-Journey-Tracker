11d10150e174818a52c11349f8f15cf635c362bb
Pooja-27 <poojarm2797@gmail.com>
2026-06-27 01:20:50 +0530

    Sliding window maximum

--- HeapProblems.java
+++ HeapProblems.java
@@ -33,27 +33,66 @@ public class HeapProblems {
             }
         }
     }
+
+    static class Pair implements Comparable<Pair> {
+        int val, idx;
+
+        public Pair(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return p2.val - this.val;
+        }
+    }
     public static void main(String[] args) {
-        int army[][] = {
-            {1, 0, 0, 0},
-            {1, 1, 1, 1},
-            {1, 0, 0, 0},
-            {1, 0, 0, 0}
-        };
-        int k = 2;
-        PriorityQueue<Row> pq = new PriorityQueue<>();
+        int arr[] = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int res[] = new int[arr.length-k+1]; //n-k+1
 
-        for(int i=0; i < army.length; i++) {
-            int count = 0;
-            for(int j=0; j < army.length; j++) {
-                count += army[i][j] == 1 ? 1 : 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        for(int i=0; i < k; i++) {
+            pq.add(new Pair(arr[i], i));
+        }
+
+        res[0] = pq.peek().val;
+        for(int i=k; i <arr.length; i++) {
+            while(pq.size() > 0 && pq.peek().idx <= (i-k)) {
+                pq.remove();
             }
-            pq.add(new Row(count, i));
+            pq.add(new Pair(arr[i], i));
+            res[i-k+1] = pq.peek().val;
         }
 
-        for(int i=0; i <k; i++) {
-            System.out.println("R" + pq.remove().idx);
+        for(int i=0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
         }
+        System.out.println();
+
+
+        // int army[][] = {
+        //     {1, 0, 0, 0},
+        //     {1, 1, 1, 1},
+        //     {1, 0, 0, 0},
+        //     {1, 0, 0, 0}
+        // };
+        // int k = 2;
+        // PriorityQueue<Row> pq = new PriorityQueue<>();
+
+        // for(int i=0; i < army.length; i++) {
+        //     int count = 0;
+        //     for(int j=0; j < army.length; j++) {
+        //         count += army[i][j] == 1 ? 1 : 0;
+        //     }
+        //     pq.add(new Row(count, i));
+        // }
+
+        // for(int i=0; i <k; i++) {
+        //     System.out.println("R" + pq.remove().idx);
+        // }
 
         // int ropes[] = {2, 3, 3, 4, 6};
         // PriorityQueue<Integer> pq = new PriorityQueue<>();
