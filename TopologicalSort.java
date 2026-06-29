17161ac3a386955785da4d14729216968036d71e
Pooja-27 <poojarm2797@gmail.com>
2026-06-29 14:14:40 +0530

    Dijkstras Algorithm

--- TopologicalSort.java
+++ TopologicalSort.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 public class TopologicalSort {
     static class Edge {
         int src, dest;
@@ -50,10 +51,43 @@ public class TopologicalSort {
         }
         stack.push(curr);
     }
+
+    public static void calcIndeg(ArrayList<Edge>[] graph, int indeg[]) {
+        for(int i=0; i < graph.length; i++) {
+            int v = i;
+            for(int j=0; j < graph[v].size(); j++) {
+                Edge e = graph[v].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void bfs(ArrayList<Edge> graph[]) {
+        int indeg[] = new int[graph.length];
+        Queue<Integer> q = new LinkedList<>();
+
+        calcIndeg(graph, indeg);
+        for(int i=0; i < indeg.length; i++) {
+            if(indeg[i] == 0) q.add(i);
+        }
+
+        while(!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.print(curr + " ");
+
+            for(int i=0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if(indeg[e.dest] == 0)q.add(e.dest);
+            }
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int v = 6;
         ArrayList<Edge> graph[] = new ArrayList[v];
         createGraph(graph);
-        topSort(graph);
+        // topSort(graph);
+        bfs(graph);
     }
 }
--- WeightedGraph.java
+++ WeightedGraph.java
@@ -50,6 +50,17 @@ public class WeightedGraph {
         stack[curr] = false;
         return false;
     }
+
+    public static void printAllPath(ArrayList<Edge> graph[], int src, int dest, String path) {
+        if(src == dest) {
+            System.out.println(path + dest);
+            return;
+        }
+        for(int i=0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            printAllPath(graph, e.dest, dest, path+src);
+        }
+    }
     public static void main(String[] args) {
         int v = 5;
         ArrayList<Edge> graph[] = new ArrayList[v];
